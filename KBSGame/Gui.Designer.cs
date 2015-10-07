ed89e1143311858233fbba6a02932fc783233a9e
edwardbomhof <edwardbomhof@live.nl>
2015-10-07 14:11:08 +0200

    guis

---
+++ KBSGame/Gui.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace KBSGame
+{
+    partial class Gui
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Gui
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Gui";
+            this.Text = "Gui";
+            this.Load += new System.EventHandler(this.Gui_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ KBSGame/Gui.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KBSGame
+{
+    public partial class Gui : Form
+    {
+        private int ID;
+        private Boolean active;
+        private Bitmap buffer;
+        public Gui(int ScreenresX, int ScreenresY)
+        {
+            this.active = true;
+            Text = "KBSGame";
+            Height = ScreenresY;
+            Width = ScreenresX;
+        }
+        public virtual Bitmap getRender()
+        {
+            return this.buffer;
+        }
+
+        public virtual Point getInput(MouseEventHandler k)
+        {
+            var relativePoint = this.PointToClient(Cursor.Position);
+            return relativePoint;
+           
+        }
+        // processes keyboard input
+        public virtual void getInput(KeyEventHandler key)
+        {
+        }
+
+
+
+        private void Gui_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
