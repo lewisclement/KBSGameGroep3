166e5a827bd643eb16324a27a5193472f7cfc4e5
Patrick Groot Koerkamp <patrick_grootkoerkamp@live.nl>
2015-10-08 12:50:18 +0200

    Sprites loaded in a functions

--- KBSGame/StaticVariables.cs
+++ KBSGame/StaticVariables.cs
@@ -5,14 +5,18 @@ namespace KBSGame
 	enum SPRITES : int {water=0, grass, sand, player, dirt, sapling1, sapling2, tallgrass, waterlily, count};
 	enum TERRAIN : int {water=0, grass, sand, dirt, count}
 	enum GUI : int {def=0, main, count};
-
-	public static class StaticVariables
+    
+    public static class StaticVariables
 	{
 		public const int tileSize = 32;
 		public const int minWorldSize = 20;
 		public const int maxWorldSize = 1000;
 		public static String execFolder = null;
 		public const Byte drawOrderSize = 16;
+        
 	}
+
+    
+			
 }
 
