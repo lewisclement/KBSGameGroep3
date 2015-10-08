16a11674c9445c4eee71fee14a67d388ba19240d
Ripy500 <github@oortgiesen.net>
2015-10-08 12:12:39 +0200

    Syntax error, missing: ,

--- KBSGame/Plant.cs
+++ KBSGame/Plant.cs
@@ -5,7 +5,7 @@ namespace KBSGame
 {
 	public class Plant : Entity
 	{
-		public Plant (int ID, Point location, int spriteID, bool solid =  true Byte depth = 8, int drawPrecision = 10)
+		public Plant (int ID, Point location, int spriteID, bool solid =  true, Byte depth = 8, int drawPrecision = 10)
             : base(ID, location, false, depth, drawPrecision)
 		{
 			this.spriteID = spriteID;
