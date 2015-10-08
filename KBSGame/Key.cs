5568f0db4b6b10897a01f86eeadafdf8d6bcf655
Patrick Groot Koerkamp <patrick_grootkoerkamp@live.nl>
2015-10-08 13:59:43 +0200

    Added empty key class + image

---
+++ KBSGame/Key.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KBSGame
+{
+    class Key : Entity
+    {
+        public Key(Point location, bool solid = false, byte height = 50, byte drawOrder = 8, int drawPrecision = 10)
+            : base(location, solid, height, drawOrder, drawPrecision)
+        {
+
+        }
+    }
+}
