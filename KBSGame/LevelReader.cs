07a3228b321d457e41a48f3f74a844fd69039856
edwardbomhof <edwardbomhof@live.nl>
2015-10-13 13:23:03 +0200

    changes

--- KBSGame/LevelReader.cs
+++ KBSGame/LevelReader.cs
@@ -29,49 +29,45 @@ namespace KBSGame
                 switch (node["type"].InnerText)
                 {
                     case "Player":
-                    try
-                    {
-                        Point location = new Point();
-                        location.X = Int32.Parse(node["X"].InnerText);
-                        location.Y = Int32.Parse(node["Y"].InnerText);
-                            // Byte height = node["height"].InnerText;
+                        try
+                        {
+                            Point location = new Point();
+                            location.X = Int32.Parse(node["X"].InnerText);
+                            location.Y = Int32.Parse(node["Y"].InnerText);
                             Player p = new Player(location, 50);
-                            this.objects.Add(p);
-                            p = null;
-                            Console.WriteLine("player");
-                        Console.WriteLine(node["ID"].InnerText);
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine("Fout bij de eerste catch");
-                    }
+                            this.objects.Add(p);                            
+                        }
+                        catch (Exception ex)
+                         {
+                            Console.WriteLine(ex.Message);
+                        }
                         break;
                     case "Terrain":
                         try
                         {
 
-                }
+                        }
                         catch (Exception ex)
-                {
+                        {
                             Console.WriteLine(ex.Message);
                         }
                         break;
                     default:
-                    try
-                    {
-                        Point location = new Point();
-                        location.X = Int32.Parse(node["X"].InnerText);
-                        location.Y = Int32.Parse(node["Y"].InnerText);
+                        try
+                        {
+                            Point location = new Point();
+                            location.X = Int32.Parse(node["X"].InnerText);
+                            location.Y = Int32.Parse(node["Y"].InnerText);
                             bool solid = Convert.ToBoolean(node["solid"].InnerText);
                             int sprite = Int32.Parse(node["spriteID"].InnerText);
                             int ID = Int32.Parse(node["ID"].InnerText);
                             Entity e = new Entity(ID, location, sprite, solid);
                             this.objects.Add(e);
-                    }
-                    catch (Exception ex)
-                    {
+                        }
+                        catch (Exception ex)
+                        {
                             Console.WriteLine(ex.Message);
-                    }
+                        }
                         break;
                 }
             }
