e3761f1e33d878ff14c55772c4740d8536b7b250
lewisclement <lewisclement@gmail.com>
2015-10-28 11:59:52 +0100

    Fixed comma now save/loaded as points

--- KBSGame/Program.cs
+++ KBSGame/Program.cs
@@ -8,6 +8,11 @@ namespace KBSGame
 		[STAThread]
 		public static void Main (string[] args)
 		{
+			System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+			customCulture.NumberFormat.NumberDecimalSeparator = ".";
+
+			System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+
 			StaticVariables.execFolder = System.IO.Path.GetDirectoryName (Application.ExecutablePath);
 			StaticVariables.levelFolder = StaticVariables.execFolder + "/worlds";
 			StaticVariables.spriteFolder = StaticVariables.execFolder + "/sprites";
