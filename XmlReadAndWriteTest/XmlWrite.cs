c1a3b097c0f02b9339fd66a77f640378b3d6974c
edwardbomhof <edwardbomhof@live.nl>
2015-10-28 16:00:26 +0100

    unit test

--- XmlReadAndWriteTest/XmlWrite.cs
+++ XmlReadAndWriteTest/XmlWrite.cs
@@ -11,7 +11,9 @@ namespace XmlReadAndWriteTest
     {
         private World w;
         private Player p;
+        private TerrainTile[] beforelist, afterlist;
         private String file = "test";
+        private World world;
 
         public XmlWrite()
         {
@@ -21,26 +23,34 @@ namespace XmlReadAndWriteTest
             // create world
             this.w = new World(10, 10);
             // fill world
-            this.w.FillWorld(TERRAIN.grass, new Size(50, 50));
+            this.w.FillWorld(TERRAIN.grass, new Size(10, 10));
             // initialize player
             this.w.InitPlayer(new PointF(5, 5));
             this.p = w.getPlayer();
-        }
-        [TestMethod]
-        public void WriteReadXml()
-        {
             LevelWriter.saveWorld(this.w, this.file);
-            TerrainTile[] beforelist, afterlist;
+
             beforelist = new TerrainTile[w.getTerrain().Count];
             // save beforlist
             w.getTerrain().CopyTo(beforelist);
-            World world = new World(10, 10);
+            world = new World(10, 10);
             world.loadLevel(this.file);
             afterlist = new TerrainTile[world.getTerrain().Count];
             // save afterlist
             world.getTerrain().CopyTo(afterlist);
+        }
+        [TestMethod]
+        public void terrainCheck()
+        {
             // checks if lists are equal
-            CollectionAssert.AreEqual(beforelist, afterlist);
+            for (int i =0; i < this.world.getTerrain().Count; i++)
+            {
+                Assert.AreEqual(this.beforelist[i].getID(),this.afterlist[0].getID());
+            }
+        }
+        [TestMethod]
+        public void entityCheck()
+        {
+            
         }
     }
 }
