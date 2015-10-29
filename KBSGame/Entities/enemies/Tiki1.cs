6259bf2de1443b12f55dc12cb4dbb3a92de9e006
lewisclement <lewisclement@gmail.com>
2015-10-29 16:30:02 +0100

    Fixed multiple enemies

--- KBSGame/Entities/enemies/Tiki1.cs
+++ KBSGame/Entities/enemies/Tiki1.cs
@@ -7,77 +7,14 @@ using System.Threading.Tasks;
 
 namespace KBSGame
 {
-    public class Tiki1 : Entity
+	public class Tiki1 : Enemy
     {
 		static int radius = 5;
 
-        public Tiki1(PointF location, Byte height = 50, bool solid = true, Byte depth = 8, float boundingBox = 0.6f)
-            : base(ENTITIES.enemy, location, (int)SPRITES.tiki1, solid, height, depth, boundingBox)
+        public Tiki1(PointF location, Byte height = 50)
+            : base(location, height, true, 8, 0.6f)
         {
 
         }
-
-        public override void onCollision()
-        {
-            StaticVariables.controller.gameover();
-        }
-
-		public void processTick()
-		{
-			PointF player = StaticVariables.world.getPlayer ().getLocation();
-
-			PointF relativeLocation = new PointF (0.0f, 0.0f);
-			if (player.X > location.X - radius && player.X < location.X + radius) {
-				if (player.X > location.X)
-					relativeLocation.X += 0.06f;
-				else if (player.X < location.X)
-					relativeLocation.X -= 0.06f;
-
-				move (relativeLocation);
-			}
-
-			relativeLocation = new PointF (0.0f, 0.0f);
-			if (player.Y > location.Y - radius && player.Y < location.Y + radius) {
-				if (player.Y > location.Y)
-					relativeLocation.Y += 0.06f;
-				else if (player.Y < location.Y)
-					relativeLocation.Y -= 0.06f;
-
-				move (relativeLocation);
-			} 
-		}
-
-		private void move(PointF relativeLocation) 
-		{
-			World world = StaticVariables.world;
-
-			float moveLocationX = location.X + relativeLocation.X;
-			float moveLocationY = location.Y + relativeLocation.Y;
-
-			PointF targetPoint = new PointF(moveLocationX, moveLocationY);
-			TerrainTile targetTile = world.getTerraintile (targetPoint);
-
-			if (world.checkCollision (this, StaticVariables.world.getPlayer ())) {
-				onCollision ();
-			}
-
-			// If terrain contains solid objects OR if tile has no walkable entity on a non-walkable tile
-			if (targetTile == null || world.checkCollision(this, targetPoint) || !targetTile.IsWalkable && !world.checkCollision(this, targetPoint, false))
-				return;
-
-			location.X = (float)Math.Round(moveLocationX, 2);
-			location.Y = (float)Math.Round(moveLocationY, 2);
-
-			//Check if the player is moving on a entity and call oncolission of that entity
-			List<Entity> entities = world.getEntities();
-
-			for(int i = 0; i < entities.Count; i++)
-			{
-				if(world.checkCollision(this, entities[i]) && entities[i].getType() != ENTITIES.enemy)
-				{
-					entities[i].onCollision();
-				}
-			}
-		}
     }
 }
